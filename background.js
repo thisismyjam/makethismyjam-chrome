ec68a45b345c2fa250743ad091654c9aba9f7783
Matthew Ogle <mogle@mac.com>
2012-08-24 22:00:43 +0100

    Better blurb, TODOs

--- background.js
+++ background.js
@@ -19,17 +19,20 @@ function jamletClicked(tab) {
 
 // Jammable URL resolver helper 
 function isPotentiallyJammable(string) {
-    if(string.match(/^(https?:\/\/)?(www\.)?soundcloud\.com\/[^\/]+\/[^\/]+/i))
-        return true; // Potential SoundCloud track page (TODO: introspect page to make sure?)
-    else if(string.match(/^(https?:\/\/)?(www\.)?youtube\.com\/watch.+/i))
+    if(string.match(/^(https?:\/\/)?(www\.)?youtube\.com\/watch.+/i))
         return true; // YouTube watch page
+    else if(string.match(/^(https?:\/\/)?(www\.)?soundcloud\.com\/[^\/]+\/[^\/]+/i)) {
+        // TODO: instrospect page to make sure?
+        return true; // Potential SoundCloud track page 
+    }
     else if(string.match(/^[^ ]+\/[^ ]+\.mp3$/))
         return true; // Found audio
     else if(string.match(/^(https?:\/\/)?(www\.)?hypem.com\/track[^\/]+/i))
         return true; // Hype Machine track page
-    else if(string.match(/^(https?:\/\/)[^\/]+\/track\//))
-        return true; // Potential Bandcamp track page (TODO: introspect page to make sure?)
-
+    else if(string.match(/^(https?:\/\/)[^\/]+\/track\//)) {
+        // TODO: Introspect page to make sure?
+        return true; // Potential Bandcamp track page 
+    }
     return false;
 }
 
