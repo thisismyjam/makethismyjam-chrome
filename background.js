9fb5b9b5fc918c0f689cbe207f3136ac769c9a4d
Aanand Prasad <aanand.prasad@gmail.com>
2012-11-16 12:36:50 +0000

    Clean up background.js a bit

--- background.js
+++ background.js
@@ -2,9 +2,8 @@
 function checkForJammableUrl(tabId, changeInfo, tab) {
   if (isPotentiallyJammable(tab.url)) {
     chrome.pageAction.show(tabId);
-  }
-  else {
-      chrome.pageAction.hide(tabId);
+  } else {
+    chrome.pageAction.hide(tabId);
   }
 };
 
@@ -13,27 +12,32 @@ chrome.tabs.onUpdated.addListener(checkForJammableUrl);
 
 // Called when jamlet icon is clicked
 function jamletClicked(tab) {
-    var destUrl = 'http://www.thisismyjam.com/jam/create?signin=1&source=jamlet&url=' + encodeURIComponent(tab.url);
-    chrome.tabs.create({'url': destUrl});
+  var destUrl = 'http://www.thisismyjam.com/jam/create?signin=1&source=jamlet&url=' + encodeURIComponent(tab.url);
+  chrome.tabs.create({'url': destUrl});
 }
 
-// Jammable URL resolver helper 
-function isPotentiallyJammable(string) {
-    if(string.match(/^(https?:\/\/)?(www\.)?youtube\.com\/watch.+/i))
-        return true; // YouTube watch page
-    else if(string.match(/^(https?:\/\/)?(www\.)?soundcloud\.com\/[^\/]+\/[^\/]+/i)) {
-        // TODO: instrospect page to make sure?
-        return true; // Potential SoundCloud track page 
-    }
-    else if(string.match(/^[^ ]+\/[^ ]+\.mp3$/))
-        return true; // Found audio
-    else if(string.match(/^(https?:\/\/)?(www\.)?hypem.com\/track[^\/]+/i))
-        return true; // Hype Machine track page
-    else if(string.match(/^(https?:\/\/)[^\/]+\/track\//)) {
-        // TODO: Introspect page to make sure?
-        return true; // Potential Bandcamp track page 
-    }
-    return false;
+function isPotentiallyJammable(url) {
+  // YouTube watch page
+  if (url.match(/^(https?:\/\/)?(www\.)?youtube\.com\/watch.+/i))
+    return true;
+  
+  // Potential SoundCloud track page (TODO: Introspect page to make sure?)
+  if (url.match(/^(https?:\/\/)?(www\.)?soundcloud\.com\/[^\/]+\/[^\/]+/i))
+    return true;
+  
+  // Found audio
+  if (url.match(/^[^ ]+\/[^ ]+\.mp3$/))
+    return true;
+  
+  // Hype Machine track page
+  if (url.match(/^(https?:\/\/)?(www\.)?hypem.com\/track[^\/]+/i))
+    return true;
+  
+  // Potential Bandcamp track page (TODO: Introspect page to make sure?)
+  if (url.match(/^(https?:\/\/)[^\/]+\/track\//))
+    return true;
+
+  return false;
 }
 
 // Listen to clicks
