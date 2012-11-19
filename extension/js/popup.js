b872b824b38cbc24f599b08828d4a1c175d2c6b8
Aanand Prasad <aanand.prasad@gmail.com>
2012-11-19 14:44:42 +0000

    Refactor popup.js to be more reactive

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,31 +1,65 @@
 Jamlet = chrome.extension.getBackgroundPage().Jamlet;
 
 Popup = {
+  status: 'initial',
+
   init: function() {
     this.element = document.getElementById('popup');
+    this.fetchHomeFeed();
+  },
 
-    var spinnerElement = document.createElement('div');
-    spinnerElement.className = 'spinner-container';
-    this.element.appendChild(spinnerElement);
-
-    var spinner = new Spinner();
-    spinner.spin(spinnerElement);
+  fetchHomeFeed: function() {
+    this.setStatus('fetchingHomeFeed');
 
     Jamlet.fetchHomeFeed(function(error, response) {
       if (error) {
         if (error.status === 401) {
-          $(this.element)
-            .html('You need to <a href="#">sign in</a>.')
-            .find('a').click(function() {
-              chrome.tabs.create({url: Jamlet.baseWebURL});
-            });
+          this.setStatus('unauthenticated');
         } else {
-          $(this.element).text('Tragically, there was an HTTP ' + error.status + ' error. Sorry.');
+          this.lastError = error;
+          this.setStatus('error');
         }
       } else {
-        this.element.innerHTML = 'There are ' + response.jams.length + ' jams in your home feed.';
+        this.homeFeed = response;
+        this.setStatus('homeFeed');
       }
     }.bind(this));
+  },
+
+  setStatus: function(status) {
+    this.status = status;
+    this.onChangeStatus();
+  },
+
+  onChangeStatus: function() {
+    switch (this.status) {
+      case 'fetchingHomeFeed':
+        var spinnerElement = document.createElement('div');
+        spinnerElement.className = 'spinner-container';
+        this.element.appendChild(spinnerElement);
+
+        var spinner = new Spinner();
+        spinner.spin(spinnerElement);
+
+        break;
+
+      case 'homeFeed':
+        this.element.innerHTML = 'There are ' + this.homeFeed.jams.length + ' jams in your home feed.';
+        break;
+
+      case 'unauthenticated':
+        $(this.element)
+          .html('You need to <a href="#">sign in</a>.')
+          .find('a').click(function() {
+            chrome.tabs.create({url: Jamlet.baseWebURL});
+          });
+
+        break;
+
+      case 'error':
+        $(this.element).text('Tragically, there was an HTTP ' + this.lastError.status + ' error. Sorry.');
+        break;
+    }
   }
 };
 
